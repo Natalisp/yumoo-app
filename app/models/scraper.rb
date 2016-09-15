8932edee0629c0bdc8345a9b271befd573742500
satub <satu.barnhill@gmail.com>
2016-09-15 17:14:47 -0400

    added model file dummy for scraping

---
+++ app/models/scraper.rb
@@ -0,0 +1,7 @@
+class Scraper
+
+  ##General New York Area
+  #https://www.yelp.com/search?find_desc=banana+split&find_loc=New+York%2C+NY&ns=1
+  ## 
+#https://www.yelp.com/search?find_desc=banana+split&find_loc=New+York,+NY&start=0&l=p:NY:New_York:Manhattan:Financial_District
+end
