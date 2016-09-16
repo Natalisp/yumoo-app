e9c5b8434c289be6a35062e4edca0c17b8e9ccfd
Flatiron School <ellis.paige.v@gmail.com>
2016-09-16 13:31:57 -0400

    removed checkboxes from home

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'profile#home'
-  post '/mood' => 'profile#mood', as: 'mood'
+  post '/home' => 'profile#mood', as: 'mood'
 end
