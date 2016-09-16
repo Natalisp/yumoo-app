a9eca52c957f53aaa26059102cad3ec92f987d97
aehmt <calisahmet@gmail.com>
2016-09-16 17:26:39 -0400

    yelp generator

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   
+  def self.yelp_instances(restaurants)
+    restaurants.each do |rest|
+      Restaurant.create(rest)
+    end
+    Restaurant.all
+  end
+
 end
