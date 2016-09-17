698e6db1f59a7a7f1f39dea57d787fdf26285a9f
Derick Quartey <niikwartey1@gmail.com>
2016-09-17 14:36:51 -0400

    name replaced with username for users

--- db/migrate/20160917012501_change_users_name_to_username.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsersNameToUsername < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :users, :name, :username
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917012501) do
+ActiveRecord::Schema.define(version: 20160916233305) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
