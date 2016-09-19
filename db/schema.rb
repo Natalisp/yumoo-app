e8fa2e466eb8496a23d58c4ba9dd53b338c65fbb
satub <satu.barnhill@gmail.com>
2016-09-19 12:16:28 -0400

    zip persists now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918032717) do
+ActiveRecord::Schema.define(version: 20160919155725) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160918032717) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.integer  "current_mood_id"
+    t.string   "zip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
