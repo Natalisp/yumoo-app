5256cb00b8bd15d523ff8a7cc28591dee43cd3cc
satub <satu.barnhill@gmail.com>
2016-09-19 10:58:55 -0400

    added fix to code break at no results from scraper

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -28,42 +28,47 @@ class Scraper
 
   def restaurant_options
     raw_text = Nokogiri::HTML(open(@scrape_address, "User-Agent" => "Mozilla/5.0 (X11; CrOS i686 12.433.216) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.105 Safari/534.30"))
-    area_of_interest = raw_text.css(".regular-search-result")
-    i = 0
-    collection = []
+    # binding.pry
+      area_of_interest = raw_text.css(".regular-search-result")
+      if area_of_interest.empty?
+        return [nil, {:error => "You live too far from this food. Move somewhere more civilized!"}]
+      else
+      i = 0
+      collection = []
 
-    while i < 6
-      restaurant = Hash.new(0)
-      #name
-      restaurant[:name] = area_of_interest.css(".indexed-biz-name")[i].css("span").text
-      #rating
-      restaurant[:rating] = area_of_interest.css(".rating-large")[i].css("i").attribute("title").value
-      #number of reviews on yelp
-      # restaurant[:reviews] = area_of_interest.css(".review-count.rating-qualifier")[i].text.match(/\d+\sreviews/)[0]
-      #price category
-      restaurant[:price] = area_of_interest.css(".business-attribute.price-range")[i].text
-      # restaurant category
-      restaurant[:category] = area_of_interest.css(".category-str-list")[i].text.gsub(/\s{2,}/,"")
-      #neighborhood
-      # restaurant[:neighborhood] = area_of_interest.css(".neighborhood-str-list")[i].text.gsub(/\s{2,}/,"")
-      #address unformatted
-      restaurant[:address] = area_of_interest.css("address")[i].text.gsub(/\s{2,}/,"")
-      #phone
-      restaurant[:phone] = area_of_interest.css(".biz-phone")[i].text.gsub(/\s{2,}/,"")
+      while i < 6
+        restaurant = Hash.new(0)
+        #name
+        restaurant[:name] = area_of_interest.css(".indexed-biz-name")[i].css("span").text
+        #rating
+        restaurant[:rating] = area_of_interest.css(".rating-large")[i].css("i").attribute("title").value
+        #number of reviews on yelp
+        # restaurant[:reviews] = area_of_interest.css(".review-count.rating-qualifier")[i].text.match(/\d+\sreviews/)[0]
+        #price category
+        restaurant[:price] = area_of_interest.css(".business-attribute.price-range")[i].text
+        # restaurant category
+        restaurant[:category] = area_of_interest.css(".category-str-list")[i].text.gsub(/\s{2,}/,"")
+        #neighborhood
+        # restaurant[:neighborhood] = area_of_interest.css(".neighborhood-str-list")[i].text.gsub(/\s{2,}/,"")
+        #address unformatted
+        restaurant[:address] = area_of_interest.css("address")[i].text.gsub(/\s{2,}/,"")
+        #phone
+        restaurant[:phone] = area_of_interest.css(".biz-phone")[i].text.gsub(/\s{2,}/,"")
 
-      #Yelp webpage
-      yelp_path = area_of_interest.css(".indexed-biz-name")[i].css("a").attribute("href").value.split("?")[0]
-      restaurant[:link] = "#{@base_address}#{yelp_path}"
+        #Yelp webpage
+        yelp_path = area_of_interest.css(".indexed-biz-name")[i].css("a").attribute("href").value.split("?")[0]
+        restaurant[:link] = "#{@base_address}#{yelp_path}"
 
-      #source
-      restaurant[:source] = "Yelp"
+        #source
+        restaurant[:source] = "Yelp"
 
-      collection[i] = restaurant
-      i = i + 1
-      #company webpage & yelp menupage (both of these require scraping the yelp webpage)!
+        collection[i] = restaurant
+        i = i + 1
+        #company webpage & yelp menupage (both of these require scraping the yelp webpage)!
+      end
+      @restaurants = collection
+      model_generator
     end
-    @restaurants = collection
-    model_generator
     #return data in a hash that can be read to restaurant model or view via controller
   end
 
